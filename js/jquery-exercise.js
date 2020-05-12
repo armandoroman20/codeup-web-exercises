05fcc95322a599b06e09460e02d398e19420b473
Armando Roman <armando.roman.jr20@gmail.com>
2020-05-12 11:45:11 -0500

    doing intro to jquery lecture

--- js/jquery-exercise.js
+++ js/jquery-exercise.js
@@ -1,5 +1,15 @@
 "use strict";
 
+
+// using an IFFE in combination with jquery DOM READY
+// (function($) {
+//     $(document).ready(function() {
+//         alert( 'The DOM has finished loading!' );
+//     });
+// })(jQuery);
+
+
 $(document).ready(function() {
     alert( 'The DOM has finished loading!' );
 });
+
