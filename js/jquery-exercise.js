c8b4b11f18223df706b0b9641369da2be4f61868
Armando Roman <armando.roman.jr20@gmail.com>
2020-05-12 14:09:28 -0500

    completed jquery exercise

--- js/jquery-exercise.js
+++ js/jquery-exercise.js
@@ -13,3 +13,11 @@
 //     alert( 'The DOM has finished loading!' );
 // });
 
+
+
+$('li').css("font-size", '20px')
+
+$('h1,p,li').css("background-color", 'yellow')
+
+
+// $('p').css('white-space', 'pre-line');
