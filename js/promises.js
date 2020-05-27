7264b682c567545b03a732101ce1eeb2f954a19d
Armando Roman <armando.roman.jr20@gmail.com>
2020-05-27 09:08:07 -0500

    completed promises exercise

--- js/promises.js
+++ js/promises.js
@@ -1,4 +1,4 @@
-"Use strict";
+"use strict";
 
 // function lastCommitDate(username, repo) {
 //     var gitHubAPI = fetch(`https://api.github.com/repos/${username}/${repo}/commits`, {headers: {'Authorization': `token ${GITHUB_API_KEY}`}});
@@ -20,20 +20,48 @@
 //
 // lastCommitDate('armandoroman20');
 
+// function wait (number) {
+//     return new Promise(resolve => setTimeout(resolve, number));
+//
+// }
+//
+// wait(1000).then(() => console.log('You\'ll see this after 1 second'));
+// wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
+
+
                             //=====Sophie's suggestion=====//
 
-
-
-
-
-
-function wait (number) {
-    return new Promise(resolve => setTimeout(resolve, number));
-
+function getLastCommitDate (username) {
+    return fetch(`https://api.github.com/users/${username}/events`, {
+        headers: {'Authorization': `token ${GITHUB_API_KEY}`}
+    }).then(response => response.json())
+        // we want to filter through push events only
+        .then(events => {
+            return events.filter(event => event.type === "PushEvent")[0].created_at;
+        });
 }
 
-wait(1000).then(() => console.log('You\'ll see this after 1 second'));
-wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
+getLastCommitDate('armandoroman20').then(date => {
+    console.log(new Date(date).toDateString())
+});
+
+
+function wait (seconds) {
+    seconds = seconds * 1000;
+    return new Promise(resolve => {
+        setTimeout(() => {
+            if (seconds > 1000) {
+                console.log("You'll see this after " + seconds/1000  + " seconds")
+            }else{
+                console.log("You'll see this after " + seconds/1000 + " seconds")
+            }
+        },seconds)
+    });
+}
+
+wait(1);
+wait(2);
+wait(3);
 
 
 
